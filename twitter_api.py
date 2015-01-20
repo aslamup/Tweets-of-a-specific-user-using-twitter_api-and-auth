ff0af64f122b722723a851267cce53120c208811
aslamup <aslamup0042@gmail.com>
2015-01-20 11:49:19 +0530

    ninth commit

--- twitter_api.py
+++ twitter_api.py
@@ -1,16 +1,19 @@
 import oauth2 as oauth
 import json
 
-CONSUMER_KEY = "4FOMZUZJuwqoRfw58TnKwNfvV"
-CONSUMER_SECRET = "9NnaR5lrM5OE8RMWRdeBvgfen5dIaFBFieO7tZbfHX8lGpgqF0"
-ACCESS_KEY = "2394103574-rEZQPhi09PvzUybydXv1Yobw7ZlTAcV7U4ItnZy"
-ACCESS_SECRET = "y4vz9cHVu3OZ3Y3z2E1Wb0Qn6XWRzb8QypaNqXdHIUs6Z"
+CONSUMER_KEY = "ENTER YOUR CONSUMER KEY"
+CONSUMER_SECRET = "ENTER YOUR CONSUMER SECRET"
+ACCESS_KEY = "ENTER YOUR ACCESS KEY"
+ACCESS_SECRET = "ENTER YOUR ACCESS SECRET"
 
 consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
 access_token = oauth.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
 client = oauth.Client(consumer, access_token)
+
 name =raw_input("Enter the User Twitter_Name: ")
-timeline_endpoint = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name="+name+"&count=20"
+nmbr =raw_input("Enter the Count of Tweets Needed: ")
+
+timeline_endpoint = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name="+name+"&count="+nmbr
 response, data = client.request(timeline_endpoint)
 
 tweets = json.loads(data)
